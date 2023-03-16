d729c15ad39d0da538bae842e6b5b597dadccaf9
whizikem <wisdomikem@gmail.com>
2023-03-16 17:15:57 +0000

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,70 +1,59 @@
 #!/usr/bin/python3
-
-""" This is a file that contains the BaseModel class for other classes """
-
+"""
+This module contains tha Base model class
+"""
+import uuid
 from datetime import datetime
-from uuid import uuid4
+import json
+import models
 
 
-class BaseModel:
+class BaseModel():
     """
-    The BaseModel class for the project
+    Base Model class
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Instantiation of the base class
+        init method
         """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
-        from models import storage
-
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-
-        else:
-            for key, value in kwargs.items():
+        if kwargs:
+            for key, val in kwargs.items():
                 if key != "__class__":
-                    if key in ("created_at", "updated_at"):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-
-    def __str__(self):
-        """
-        Returns the string representation of an object
-        Format:
-        [<class name>] (<self.id>) <self.__dict__>
-        """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id,
-                                     self.__dict__)
+                    if key == "created_at" or key == "updated_at":
+                        val = datetime.strptime(val, time_format)
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            x = models.storage.new(self)
 
     def save(self):
         """
-        updates the public instance attribute updated_at with the current
-        datetime
+        saving public instance attribute
         """
-
-        from models import storage
-
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
+
+    def __str__(self):
+        """
+        Ptinting class name, id and dict
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the
-        instance
-        + by using self.__dict__, only instance attributes set will be returned
-        + a key __class__ must be added to this dictionary with the class name
-        of the object
+        Adding to dictionary
         """
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = self.__class__.__name__
-
+        dic = {}
         for key, value in self.__dict__.items():
-            if key in ("created_at", "updated_at"):
-                value = self.__dict__[key].isoformat()
-                dictionary[key] = value
-
-        return dictionary
+            dic["__class__"] = self.__class__.__name__
+            if key == "updated_at" or key == "created_at":
+                dic[key] = value.isoformat()
+            else:
+                dic[key] = value
+        return dic
