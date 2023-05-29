385076c2254b188429678006b208e60e88040a91
whizikem <wisdomikem@gmail.com>
2023-05-29 17:59:52 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,30 +2,39 @@
 # Bash script that sets up your web servers for the deployment of web_static
 
 sudo apt-get update
-sudo apt-get -y install nginx
-sudo service nginx start
-sudo apt-get install ufw
-sudo ufw allow 'Nginx HTTP'
+sudo apt-get install nginx
 
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web-static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/relaeases/test/ /data/web_static/current
+file_path="/data/web_static/releases/test/index.html"
+# Create the directory if it doesn't exist
+sudo mkdir -p "$(dirname "$file_path")"
+# Create the fake HTML file
+sudo echo "<html>
+<head>
+</head>
+<body>
+<h1>Holberton School</h1>
+</body>
+</html>" > "file_path"
+
+link_path="/data/web_static/current"
+target_path="/data/web_static/releases/test/"
+# Check if the symbolic link exists
+if [ -L "$link_path" ]; then
+    # Delete the existsing symbolic link
+    rm "$link_path"
+fi
+# Create the symbolic link
+sudo ln -s "$target_path" "$link_path"
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name .*;/a location /hbnb_static/ {\nalias /data/web_static/current/;\nindex index.html;\n}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
 
