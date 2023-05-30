323e018eeb1b29e3fb21692b7f90313993d64fdd
whizikem <wisdomikem@gmail.com>
2023-05-30 22:50:15 +0000

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,35 +3,36 @@
 Fabric script that distributes an archive to your web servers
 using the function do_deploy
 """
-from fabric.api import env, run, put, hosts
-# from datetime import datetime
-import os
+from fabric.api import env, put, run
+from os.path import exists
 
 env.hosts = ['100.25.117.48', '54.146.58.123']
-
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    """
-    upload the archive to the /tmp/ directory of the web server
-    uncompress the archive
-    delete the archive from the web server
-    handle symbolic links
-    return True is all operations went well, False otherwise
-    """
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
+
     try:
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1]
-        file_name_noext = file_name.split('.')[0]
-        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
-        run('sudo mkdir -p {}'.format(new_folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
-        run('sudo rm -rf {}web_static'.format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
+
+        # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
+        filename = archive_path.split('/')[-1]
+        folder_name = '/data/web_static/releases/' + filename.split('.')[0]
+        run('mkdir -p {}'.format(folder_name))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
+        
+        # Remove the archive from the web server
+        run('rm /tmp/{}'.format(filename))
+        
+        # Delete the symbolic link /data/web_static/current
+        run('rm -f /data/web_static/current')
+
+        # Create a new symbolic link /data/web_static/current to the new version of the code
+        run('ln -s {} /data/web_static/current'.format(folder_name))
+
         print("New version deployed!")
         return True
     except:
