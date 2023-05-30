3836c32da1ce2f73278d84773f278601899318cd
whizikem <wisdomikem@gmail.com>
2023-05-30 21:16:55 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,9 @@ Fabric script that generates a .tgz archive from the contents of the web_static 
 from fabric.api import local
 from datetime import datetime
 
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder.
-    """
+"""def do_pack():
+    Generates a .tgz archive from the contents of the web_static folder.
+    
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(timestamp)
@@ -20,4 +20,19 @@ def do_pack():
     if result.failed:
         return None
     else:
-        return archive_path
+        return archive_path"""
+
+def do_pack():
+    """
+    create the archive file with the contents of the web_static folder
+    and return the archive path if the archive has been correctly generated
+    otherwise return None
+    """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(datetime_str)
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(file_name))
+        return "versions/{}".format(file_name)
+    except:
+        return None
