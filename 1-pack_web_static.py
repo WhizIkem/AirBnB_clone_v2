97a496100e1e85e1b0ce1eac86a8f5bf11b2422d
whizikem <wisdomikem@gmail.com>
2023-05-30 21:30:30 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -22,17 +22,19 @@ from datetime import datetime
     else:
         return archive_path"""
 
+from time import strftime
+from datetime import date
+
 def do_pack():
-    """
-    create the archive file with the contents of the web_static folder
-    and return the archive path if the archive has been correctly generated
-    otherwise return None
-    """
-    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "web_static_{}.tgz".format(datetime_str)
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
     try:
-        local('mkdir -p versions')
-        local('tar -cvzf versions/{} web_static'.format(file_name))
-        return "versions/{}".format(file_name)
-    except:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
