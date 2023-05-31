f0d17ab872521a4dc5d6dc858dd4e4472a30e067
whizikem <wisdomikem@gmail.com>
2023-05-31 09:29:07 +0000

    Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,84 +1,47 @@
-class web_servers {
-  package { 'nginx':
-    ensure => installed,
-  }
+# Prepare web servers
 
-  file { '/data/':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    recurse => true,
-  }
-
-  file { '/data/web_static/':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-  }
-
-  file { '/data/web_static/releases/':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-  }
-
-  file { '/data/web_static/shared/':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-  }
-
-  file { '/data/web_static/releases/test/':
-    ensure  => directory,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-  }
-
-  file { '/data/web_static/releases/test/index.html':
-    ensure  => file,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    content => '<html><head></head><body>Holberton School</body></html>',
-  }
-
-  file { '/data/web_static/current':
-    ensure => link,
-    target => '/data/web_static/releases/test/',
-    force  => true,
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        server_name _;
-
-        location /hbnb_static/ {
-          alias /data/web_static/current/;
-        }
-
-        location / {
-          return 404;
-        }
-      }
-    ",
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-    require   => Package['nginx'],
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
+exec { 'update':
+  command => '/usr/bin/apt-get update',
 }
 
-include web_servers
+package { 'nginx':
+  ensure  => installed,
+}
 
+file { ['/data/', '/data/web_static/', '/data/web_static/releases/', '/data/web_static/releases/test', '/data/web_static/shared']:
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  content => "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>",
+    owner => 'ubuntu',
+    group => 'ubuntu',
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+  force  => yes,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+exec { 'sed':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location \
+/hbnb_static/ { alias /data/web_static/current/;}" \
+/etc/nginx/sites-available/default',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
