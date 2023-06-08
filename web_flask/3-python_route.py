a39eaf952880b070f926f460e5b31dea6636d735
whizikem <wisdomikem@gmail.com>
2023-06-08 15:24:30 +0000

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,57 +4,29 @@ Script that starts a Flask web application
 listening on 0.0.0.0 port 5000
 """
 
-"""from flask import Flask, request
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
-"""
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
-
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_hbnb():
-    return 'Hello HBNB!'
-
-@app.route('/hbnb')
-def hbnb():
-    return 'HBNB'
-
-@app.route('/c/<text>')
-def c(text):
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python(text='is cool'):
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
-
