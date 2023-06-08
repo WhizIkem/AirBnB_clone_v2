994a0e78ac0f2b9c1713e9519d9459ad975c284b
whizikem <wisdomikem@gmail.com>
2023-06-08 15:47:37 +0000

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,13 +7,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
