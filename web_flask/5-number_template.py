0748e03d6075f98b8a221eeacdac131b6bafb404
whizikem <wisdomikem@gmail.com>
2023-06-08 17:39:33 +0000

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,8 +37,8 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', number=n)
+def number_template(n=None):
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
