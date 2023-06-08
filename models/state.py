012526a7d5f512aa1760139129eb81dd45cd9335
whizikem <wisdomikem@gmail.com>
2023-06-08 23:05:07 +0000

    update

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from os import getenv
-from models import storage
+import sqlalchemy
 
 
 class State(BaseModel, Base):
@@ -21,6 +21,11 @@ class State(BaseModel, Base):
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """getter - returns the list of City instances"""
@@ -29,7 +34,7 @@ class State(BaseModel, Base):
                     if obj.state_id == self.id]"""
             """ Returns the list of City objects from storage linked to the current State"""
             city_list = []
-            for city in storage.all("City").values():
+            for city in models.storage.all("City").values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
