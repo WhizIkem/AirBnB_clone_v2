a914fd161c4866f345dd5fb13cf1db78c9b653bb
whizikem <wisdomikem@gmail.com>
2023-06-15 14:47:05 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
